7b46d9cff0b69ee9fe8bb8ad0f5f6b455eff4345
seyamasan <kst_apr_24@icloud.com>
2025-06-03 17:14:22 +0900

    Initial Commit

---
+++ SwiftTestingSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTestingSampleApp
+//
+//  Created by 瀬山皐太 on 2025/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTestingSampleApp/SwiftTestingSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingSampleAppApp.swift
+//  SwiftTestingSampleApp
+//
+//  Created by 瀬山皐太 on 2025/06/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
