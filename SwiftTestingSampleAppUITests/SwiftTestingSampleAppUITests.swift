b4e4dfaa46ac891fbb912560d52a18188786e0bc
seyamasan <kst_apr_24@icloud.com>
2025-06-22 21:17:18 +0900

    test: Performance tests were removed.

--- SwiftTestingSampleAppUITests/SwiftTestingSampleAppUITests.swift
+++ SwiftTestingSampleAppUITests/SwiftTestingSampleAppUITests.swift
@@ -170,14 +170,4 @@ final class SwiftTestingSampleAppUITests: XCTestCase {
         XCTAssertFalse(errorAlertMessage.exists)
         XCTAssertFalse(errorAlertOkButton.exists)
     }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
